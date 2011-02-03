d4914a80c66cb126b17ecef8afb5b5ee669c2f3a
Nick Markwell <nick@duckinator.net>
2011-02-03 04:21:33 -0500

    Yey strings

---
+++ test/parser/strings.rb
@@ -0,0 +1,27 @@
+require 'polyglot'
+require 'treetop'
+
+Treetop.load File.join(File.dirname(__FILE__), '..', '..', 'lib', 'TDL')
+
+parser = TDLParser.new
+
+describe TDLParser do
+  describe "#parse" do
+    describe "strings" do
+      specify { parser.parse('"abcd"'    ).should be     } # "abcd"
+      specify { parser.parse('"abc\\d"'  ).should be     } # "abc\d"
+      specify { parser.parse('"abc\\"d"' ).should be     } # "abc\"d"
+      specify { parser.parse('"\\"abcd"' ).should be     } # "\"abcd"
+      specify { parser.parse('"abc\r\nd"').should be     } # "abc\r\nd"
+      specify { parser.parse('"abc\td\\"').should_not be } # "abc\td\"
+
+      specify { parser.parse("'abcd'"    ).should be     } # 'abcd'
+      specify { parser.parse("'abc\\d'"  ).should be     } # 'abc\d'
+      specify { parser.parse("'abc\\'d'" ).should be     } # 'abc\"d'
+      specify { parser.parse("'\\'abcd'" ).should be     } # '\"abcd'
+      specify { parser.parse("'abc\\r\\nd'").should be     } # 'abc\r\nd'
+      specify { parser.parse("'abc\\td\\'").should_not be } # 'abc\td\'
+    end
+  end
+end
+
